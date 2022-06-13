4cdfec8e1e204ef5c1c7bea75e984e97b8acbbe0
alexandr <gerasimov707@yandex.ru>
2022-06-13 20:25:03 +0300

    2.7 дз

--- src/main/java/compass/homework_2_5/service/EmployeeService.java
+++ src/main/java/compass/homework_2_5/service/EmployeeService.java
@@ -6,46 +6,49 @@ import compass.homework_2_5.exceptions.EmployeeStorageIsFullExceptions;
 import compass.homework_2_5.model.Employee;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeService {
+
     private static final int LIMIT = 10;
 
-    private final List<Employee> employees = new ArrayList<>();
+    private final Map<String, Employee> employees = new HashMap<>();
 
-    public Employee add(String firstname, String lastname) {
-        Employee employee = new Employee(firstname, lastname);
-        if (employees.contains(employee)) {
-            throw new EmployeeAlreadyAddedExceptions();
-        }
-        if (employees.size() < LIMIT) {
-            employees.add(employee);
-            return employee;
-        }
-        throw new EmployeeStorageIsFullExceptions();
+    private String getKey(Employee employee){
+        return employee.getName()+"|" +employee.getSurname();
     }
-        public Employee remove (String firstname, String lastname) {
-            Employee employee = new Employee(firstname, lastname);
-            if (!employees.contains(employee)) {
-                throw new EmployeeNotFindExceptions();
-            }
-                    employees.remove(employee);
-                    return employee;
-                }
 
-        public Employee find (String firstname, String lastname){
-            Employee employee = new Employee(firstname, lastname);
-                if (!employees.contains(employee)) {
-                    throw new EmployeeNotFindExceptions();
-                }
-                    return employee;
-                }
-        public List<Employee> getAll() {
-            return new ArrayList<>(employees);
+    public Employee add(String name, String surname) {
+        Employee employee = new Employee(name, surname);
+        String key = getKey(employee);
+        if (employees.containsKey(key)){
+            throw new EmployeeAlreadyAddedException();
         }
-}
+        if (employees.size()<LIMIT){
+            return employees.put(key, employee);
+        }
+        throw new EmployeeStorageIsFullException();
+    }
 
+    public Employee remove(String name, String surname) {
+        String key = getKey(new Employee(name, surname));
+        if (!employees.containsKey(key)){
+            throw new EmployeeNotFoundException();
+        }
+        return employees.remove(key);
+    }
+
+    public Employee find(String name, String surname) {
+        Employee employee = new Employee(name, surname);
+        if (!employees.containsKey(getKey(employee))){
+            throw new EmployeeNotFoundException();
+        }
+        return employee;
+    }
+
+    public List<Employee> getAll(){
+        return new ArrayList<>(employees.values());
+    }
+}
 
